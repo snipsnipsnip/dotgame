2c2b39f7574eb6e2a0a66ab8ac8cd4f1fca26959
snip <s.skk.skk@gmail.com>
2011-05-09 15:58:37 +0900

    implemented image and text

--- dotgame.rb
+++ dotgame.rb
@@ -78,13 +78,153 @@ end
 # 線を引く
 #
 # 書式：
-#   square もとのX座標, もとのY座標, 横幅, 縦幅
+#   square 左上のX座標, 左上のY座標, 横幅, 縦幅
 #
 # 例：
 #   square 10, 10, 5, 5, blue
 def square(x, y, w, h, r=0, g=nil, b=nil, a=255, s=StarRuby::Game.current.screen)
   color = r.is_a?(StarRuby::Color) ? r : Color(r, g || r, b || r, a)
   s.render_rect x, y, w, h, color
+end
+
+# 画像を張り付ける
+# フォルダ越しに指定する場合 \ ではなく / を使うこと
+#
+# 書式：
+#   image "ファイル名"
+#   image "ファイル名", 左上のX座標, 左上のY座標
+#   image "ファイル名", 左上のX座標, 左上のY座標, 倍率
+#
+# 例：
+#   image "neko.bmp"
+#   image "c:/tree.gif", 5, 5
+#   image "c:/face.png", 3, 3, 2
+def image(name, x=0, y=0, scale=nil, s=StarRuby::Game.current.screen)
+  opt = scale.is_a?(Hash) ? scale : { :scale_x => scale, :scale_y => scale } if scale
+  if t = name.is_a?(String) ? DotGame.get_texture(name) : name
+    s.render_texture t, x, y, opt
+  else
+    warn "\211\346\221\234 '#{name}' \202\252\223\307\202\335\215\236\202\337\202\334\202\271\202\361\202\305\202\265\202\275"
+  end
+end
+
+def self.get_texture(name)
+  @_textures ||= {}
+  @_textures[name] ||= StarRuby::Texture.load(name)
+end
+
+# 英数字を書く
+#
+# 書式：
+#   text 変数名
+#   text 数値
+#   text "メッセージ"
+#   text "メッセージ", 左上のX座標, 左上のY座標
+#   text "メッセージ", 左上のX座標, 左上のY座標, 色の名前 (red, blue, green..)
+#   text "メッセージ", 左上のX座標, 左上のY座標, 色の濃さ (0～255)
+#   text "メッセージ", 左上のX座標, 左上のY座標, R, G, B
+#
+# 例：
+#   text 100
+#   text "hoge"
+#   text "hoge", 5, 5
+def text(msg, x=0, y=0, r=0, g=nil, b=nil, a=255, s=StarRuby::Game.current.screen)
+  color = r.is_a?(StarRuby::Color) ? r : Color(r, g || r, b || r, a)
+  ix = x
+  msg.to_s.each_byte do |c|
+    if c == 10
+      x = ix
+      y += 6
+    else
+      DotGame.draw_letter(s, x, y, c, color)
+      x += 6
+    end
+  end
+end
+
+# フチつきtext
+def textbold(msg, x=0, y=0, inner=white, outer=black, s=StarRuby::Game.current.screen)
+  x += 1
+  y += 1
+  ix = x
+  msg.to_s.each_byte do |c|
+    if c == 10
+      x = ix
+      y += 7
+    else
+      DotGame.draw_letter_bold(s, x, y, c, inner, outer)
+      x += 8
+    end
+  end
+end
+
+# 5x5 bitmap font by shinh (http://d.hatena.ne.jp/shinichiro_h/20060814#1155567183)
+# Licensed under the New BSD License.
+ShinhFont = [
+    0x00000000, 0x00401084, 0x0000014a, 0x00afabea, 0x01fa7cbf, 0x01111111,
+    0x0126c8a2, 0x00000084, 0x00821088, 0x00221082, 0x015711d5, 0x00427c84,
+    0x00220000, 0x00007c00, 0x00400000, 0x00111110, 0x00e9d72e, 0x00421084,
+    0x01f2222e, 0x00e8b22e, 0x008fa54c, 0x01f87c3f, 0x00e8bc2e, 0x0042221f,
+    0x00e8ba2e, 0x00e87a2e, 0x00020080, 0x00220080, 0x00820888, 0x000f83e0,
+    0x00222082, 0x0042222e, 0x00ead72e, 0x011fc544, 0x00f8be2f, 0x00e8862e,
+    0x00f8c62f, 0x01f0fc3f, 0x0010bc3f, 0x00e8e42e, 0x0118fe31, 0x00e2108e,
+    0x00e8c210, 0x01149d31, 0x01f08421, 0x0118d771, 0x011cd671, 0x00e8c62e,
+    0x0010be2f, 0x01ecc62e, 0x0114be2f, 0x00f8383e, 0x0042109f, 0x00e8c631,
+    0x00454631, 0x00aad6b5, 0x01151151, 0x00421151, 0x01f1111f, 0x00e1084e,
+    0x01041041, 0x00e4210e, 0x00000144, 0x01f00000, 0x00000082, 0x0164a4c0,
+    0x00749c20, 0x00e085c0, 0x00e4b908, 0x0060bd26, 0x0042388c, 0x00c8724c,
+    0x00a51842, 0x00420080, 0x00621004, 0x00a32842, 0x00421086, 0x015ab800,
+    0x00949800, 0x0064a4c0, 0x0013a4c0, 0x008724c0, 0x00108da0, 0x0064104c,
+    0x00c23880, 0x0164a520, 0x00452800, 0x00aad400, 0x00a22800, 0x00111140,
+    0x00e221c0, 0x00c2088c, 0x00421084, 0x00622086, 0x000022a2, 
+]
+
+def self.draw_letter(screen, x, y, c, color)
+  i = c - 32
+  return if i < 0 || i >= ShinhFont.size
+  d = ShinhFont[i]
+  
+  5.times do |i|
+    5.times do |j|
+      next if ((d >> (i * 5 + j)) & 1) == 0
+      
+      screen[x + j, y + i] = color
+    end
+  end
+end
+
+def self.draw_letter_bold(screen, x, y, c, inner, outer)
+  i = c - 32
+  return if i < 0 || i >= ShinhFont.size
+  d = ShinhFont[i]
+  
+  5.times do |i|
+    5.times do |j|
+      next if ((d >> (i * 5 + j)) & 1) == 0
+      
+      screen[x + j, y + i] = inner
+      
+      # top
+      if i == 0 || ((d >> ((i - 1) * 5 + j)) & 1) == 0
+        screen[x + j, y + i - 1] = outer
+      end
+
+      # bottom
+      if i == 4 || ((d >> ((i + 1) * 5 + j)) & 1) == 0
+        screen[x + j, y + i + 1] = outer
+      end
+      
+      # left
+      if j == 0 || ((d >> (i * 5 + j - 1)) & 1) == 0
+        screen[x + j - 1, y + i] = outer
+      end
+      
+      # right
+      if j == 4 || ((d >> (i * 5 + j + 1)) & 1) == 0
+        screen[x + j + 1, y + i] = outer
+      end
+    end
+  end
 end
 
 # 入力とか #
@@ -234,12 +374,12 @@ end
 
 # 本体 #
 
-def main(fps=30, w=20, h=20)
+def main(w=20, h=20, fps=30)
   bg = StarRuby::Texture.new(w, h)
   raster(bg) do |x,y|
     bg[x, y] = (x + y).odd? ? white : gray
   end
-  StarRuby::Game.run(w, h, :window_scale => [600 / [w, h].min, 1].max, :cursor => true, :fps => fps) do |game|
+  StarRuby::Game.run(w, h, :window_scale => [600.0 / [w, h].max, 1].max, :cursor => true, :fps => fps) do |game|
     game.screen.render_texture(bg, 0, 0)
     yield
   end
