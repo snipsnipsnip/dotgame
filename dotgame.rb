938a4cfca928ab5c90a5463c3def6e469dd2a253
snip <s.skk.skk@gmail.com>
2011-05-09 16:26:20 +0900

    implemented playse and playbgm

--- dotgame.rb
+++ dotgame.rb
@@ -1,11 +1,11 @@
-# dotgame r2
+# dotgame: starruby for dummies
 # https://gist.github.com/962107
 
 require 'starruby'
 
 module DotGame
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 
 include Math
 
@@ -224,6 +224,52 @@ def self.draw_letter_bold(screen, x, y, c, inner, outer)
         screen[x + j + 1, y + i] = outer
       end
     end
+  end
+end
+
+# 音 #
+
+# 効果音を鳴らす
+# 数値を指定すると再生を止める
+#
+# 書式：
+#   playse フェードアウト時間
+#   playse "ファイル名"
+#   playse "ファイル名", 音量 (0～255)
+#   playse "ファイル名", 音量 (0～255), パン (-255～255)
+#
+# 例：
+#   playse "cluck.wav"
+#   playse "dump.ogg"
+def playse(name, vol=nil, pan=nil)
+  if name.is_a?(Integer)
+    StarRuby::Audio.stop_all_ses({ :time => name })
+  else
+    opt = { :volume => vol || 255, :panning => pan || 0 } if vol || pan
+    StarRuby::Audio.play_se name.to_s, opt
+  end
+end
+
+# BGMを鳴らす
+# 数値を指定すると再生を止める
+#
+# 書式：
+#   playbgm フェードアウト時間 (ミリ秒)
+#   playbgm "ファイル名"
+#   playbgm "ファイル名", 音量 (0～255)
+#   playbgm "ファイル名", 音量 (0～255), フェードイン時間 (ミリ秒)
+#
+# 例：
+#   playbgm "steelpython.ogg"
+#   playbgm "discretemusic.ogg", 50
+#   playbgm "landau.ogg", 255, 10000
+#   playbgm 1000
+def playbgm(name, volume=255, fadein=0)
+  if name.is_a?(Integer)
+    StarRuby::Audio.stop_bgm({ :time => name })
+  else
+    opt = { :loop => true, :volume => volume, :time => fadein }
+    StarRuby::Audio.play_bgm name.to_s, opt
   end
 end
 
