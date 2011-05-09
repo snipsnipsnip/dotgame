fb63c9661d84c6df1985b51d082436b5a03a00cd
snip <s.skk.skk@gmail.com>
2011-05-09 15:16:43 +0900

    もじばけなおしてsquareとか追加

--- dotgame.rb
+++ dotgame.rb
@@ -1,7 +1,13 @@
-# dotgame r1
+# dotgame r2
 # https://gist.github.com/962107
 
 require 'starruby'
+
+module DotGame
+
+VERSION = "0.0.2"
+
+include Math
 
 # 色 #
 
@@ -48,66 +54,102 @@ end
 #   dot 3, 3
 #   dot 10, 10, 50
 #   dot 10, 10, 50, 200, 50
-def dot(x, y, r=0, g=nil, b=nil, a=255)
+def dot(x, y, r=0, g=nil, b=nil, a=255, s=StarRuby::Game.current.screen)
   color = r.is_a?(StarRuby::Color) ? r : Color(r, g || r, b || r, a)
-  StarRuby::Game.current.screen[x, y] = color
+  s.render_pixel x, y, color
+end
+
+# 線を引く
+#
+# 書式：
+#   line もとのX座標, もとのY座標, 先のX座標, 先のY座標
+#   line もとのX座標, もとのY座標, 先のX座標, 先のY座標
+#   line もとのX座標, もとのY座標, 先のX座標, 先のY座標, 色の名前 (red, blue, green..)
+#   line もとのX座標, もとのY座標, 先のX座標, 先のY座標, 色の濃さ (0～255)
+#   line もとのX座標, もとのY座標, 先のX座標, 先のY座標, R, G, B
+#
+# 例：
+#   line 10, 10, 50, 200, red
+def line(x1, y1, x2, y2, r=0, g=nil, b=nil, a=255, s=StarRuby::Game.current.screen)
+  color = r.is_a?(StarRuby::Color) ? r : Color(r, g || r, b || r, a)
+  s.render_line x1, y1, x2, y2, color
+end
+
+# 線を引く
+#
+# 書式：
+#   square もとのX座標, もとのY座標, 横幅, 縦幅
+#
+# 例：
+#   square 10, 10, 5, 5, blue
+def square(x, y, w, h, r=0, g=nil, b=nil, a=255, s=StarRuby::Game.current.screen)
+  color = r.is_a?(StarRuby::Color) ? r : Color(r, g || r, b || r, a)
+  s.render_rect x, y, w, h, color
 end
 
 # 入力とか #
 
 # スペースキー
-def space
+def space?
   key?(:space)
 end
 
 # アルファベット各種
-def zkey
+def zkey?
   key?(:z)
 end
 
-def xkey
+def xkey?
   key?(:x)
 end
 
-def ckey
+def ckey?
   key?(:c)
 end
 
-def vkey
+def vkey?
   key?(:v)
 end
 
-def akey
+def akey?
   key?(:a)
 end
 
-def skey
+def skey?
   key?(:s)
 end
 
 # 矢印
-def left
+def left?
   key?(:left)
 end
 
-def right
+def right?
   key?(:right)
 end
 
-def up
+def up?
   key?(:up)
 end
 
-def down
+def down?
   key?(:down)
 end
 
-def leftclick
+def leftmouse?
   key?(:left, :mouse)
 end
 
-def rightclick
+def rightmouse?
   key?(:left, :mouse)
+end
+
+def leftclick?
+  click?(:left, :mouse)
+end
+
+def rightclick?
+  click?(:left, :mouse)
 end
 
 def mousex
@@ -132,13 +174,31 @@ def Color(r, g, b, a=255)
   StarRuby::Color.new(r, g, b, a)
 end
 
-# キーを調べる
+# キーが押されてる最中ならtrue、でなければfalse
 # 例:
 #   if key?(:z)
 #     # Zボタン押してる時の処理
 #   end
 def key?(key, device=:keyboard)
   StarRuby::Input.keys(device).include?(key)
+end
+
+# キーが放された瞬間ならtrue、でなければfalse
+# 例:
+#   if click?(:left, :mouse)
+#     # Zボタンが離された瞬間だけの処理
+#   end
+def click?(key, device=:keyboard)
+  new = key?(key, device)
+  old = DotGame._click_state([key, device], new)
+  old && !new
+end
+
+def self._click_state(key, new)
+  @_click_state ||= {}
+  old = @_click_state[key]
+  @_click_state[key] = new
+  old
 end
 
 # 全画面一気に描画する
@@ -174,30 +234,25 @@ end
 
 # 本体 #
 
-def self.main(fps=10, w=20, h=20)
-  StarRuby::Game.run(w, h, :window_scale => [w, h].max * 30 / 20, :cursor => true, :fps => fps) do |game|
-    raster(game.screen) do |x,y|
-      dot x, y, (x + y).odd? ? white : gray
-    end
+def main(fps=30, w=20, h=20)
+  bg = StarRuby::Texture.new(w, h)
+  raster(bg) do |x,y|
+    bg[x, y] = (x + y).odd? ? white : gray
+  end
+  StarRuby::Game.run(w, h, :window_scale => [600 / [w, h].min, 1].max, :cursor => true, :fps => fps) do |game|
+    game.screen.render_texture(bg, 0, 0)
     yield
   end
 end
 
+end
+
 if __FILE__ == $0
   if File.exist?('main.txt')
-    include Math
+    include DotGame
     load 'main.txt'
   else
     require 'Win32API'
-    Win32API.new('user32', 'MessageBox', %w(p p p i), 'i').call(0, <<EOS, "dotgame", 0)
-このファイルと同じところにmain.txtを置いてください
-内容は以下の通りです
-
-main() {
-}
-
-詳しい解説は以下のサイトでどうぞ
-  http://m-creator.vbl.oita-u.ac.jp/kodama/moin/dotgame/
-EOS
+    Win32API.new('user32', 'MessageBox', %w(p p p i), 'i').call(0, "\202\261\202\314\203t\203@\203C\203\213\202\306\223\257\202\266\202\306\202\261\202\353\202\311main.txt\202\360\222u\202\242\202\304\202\255\202\276\202\263\202\242\n\223\340\227e\202\315\210\310\211\272\202\314\222\312\202\350\202\305\202\267\n\nmain() {\n}\n\n\217\332\202\265\202\242\211\360\220\340\202\315\210\310\211\272\202\314\203T\203C\203g\202\305\202\307\202\244\202\274\n  http://m-creator.vbl.oita-u.ac.jp/kodama/moin/dotgame/\n", "dotgame", 0)
   end
 end
