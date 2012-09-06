7c2edec1f7b9c7060ebfd8245837ad0d803bce0b
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2012-09-06 13:23:56 +0900

    test for c6c33f5c3e

--- test/draw_test.rb
+++ test/draw_test.rb
@@ -17,23 +17,33 @@ class DrawTest < Test::Unit::TestCase
     end
     
     context "text" do
-      should "accept 1 argument" do
-        font = DotGame::DotFont.any_instance
-        black = stub("black")
-        
-        called = []
-        font.stubs(:draw_letter).with {|*args| called << args }.times(4)
-        subject.stubs(:Color).with(0, 0, 0, 255).returns(black)
-        
-        subject.text "hoge"
-        
-        called.each_with_index do |(screen, x, y, char, scale, color), i|
-          assert_equal subject, screen
-          assert_equal 6 * i, x
-          assert_equal 0, y
-          assert_equal "hoge"[i] - 32, char
-          assert_equal 1, scale
-          assert_equal black, color
+      setup do
+        @draw_letter_calls = []
+        @any_font = DotGame::DotFont.any_instance
+        @any_font.stubs(:draw_letter).with {|*args| @draw_letter_calls << args }
+        @color = stub("color")
+        subject.stubs(:Color).with(0, 0, 0, 255).returns(@color)
+      end
+      
+      teardown do
+        @any_font.unstub(:draw_letter)
+      end
+    
+      args = ["hoge", 0, 0, 1, 0, 0, 0, 255]
+      
+      args.each_index do |i|
+        should "accept #{i + 1} argument(s)" do
+          subject.text *args[0..i]
+          
+          assert_equal 4, @draw_letter_calls.size
+          @draw_letter_calls.each_with_index do |(screen, x, y, char, scale, color), i|
+            assert_equal subject, screen
+            assert_equal 6 * i, x
+            assert_equal 0, y
+            assert_equal "hoge"[i] - 32, char
+            assert_equal 1, scale
+            assert_equal @color, color
+          end
         end
       end
     end
